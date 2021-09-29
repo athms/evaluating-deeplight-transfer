ea611fc3b1654a03a8550118713574ee6500c54e
athms <armin.thms@gmail.com>
2021-09-29 08:46:01 -0700

    cleanup

--- deeplight/three/_fit.py
+++ deeplight/three/_fit.py
@@ -10,7 +10,6 @@ def _make_callbacks(output_path: str):
   filepath = output_path+"epoch-{epoch:03d}.h5"
   checkpoint = tf.keras.callbacks.ModelCheckpoint(
   filepath, monitor='val_acc', verbose=1, save_best_only=False, period=1)
-  # csv
   csv_logger = tf.keras.callbacks.CSVLogger(output_path+'history.csv', separator=",", append=True)
   return [checkpoint, csv_logger]
 
