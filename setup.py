98aca2223b415f3cc8a691edd70d79bfa3fb4966
athms <armin.thms@gmail.com>
2021-09-30 20:57:31 -0700

    code formatting

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     name='deeplight',
     version='0.1.0',
     description='deeplight package',
-    author='Armin Thomas',
+    author='Armin W. Thomas',
     author_email='athms.research@gmail.com',
     packages=['deeplight', 'hcprep', 'modules'],
-    license='MIT')
+    license='MIT'
+)
